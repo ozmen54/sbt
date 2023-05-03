6023f2ef3d4a2b74a1abee1dc62612d065fed23c
ozmen54 <ozmen@sakarya.edu.tr>
2023-05-03 17:45:22 +0300

    Something has changed.

--- src/main/java/com/sau/sbt/Controllers/HomeController.java
+++ src/main/java/com/sau/sbt/Controllers/HomeController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HomeController {
     @GetMapping("/")
     public String getIndex(){
-        return "Hello CI-CD";
+        return "Hello. This is OK.";
     }
 }
